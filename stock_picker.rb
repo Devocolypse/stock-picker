d8797d7bd4901860beae28114366acc3265e4d0c
Devocolypse <72180176+Devocolypse@users.noreply.github.com>
2025-05-14 10:00:34 -0700

    feat: Return an array of best buy_day and sell_day

--- stock_picker.rb
+++ stock_picker.rb
@@ -3,15 +3,22 @@ def stock_picker(stocks)
   sell_day = 0
   best_profit = 0
 
-  stocks.each_with_index do |price, day|
+  stocks.each_with_index do |buy_price, day|
     comparison = 1
+    stocks.each_with_index do |comp_price, comp_day|
+      next unless (comp_day == day + comparison) && (comp_price > buy_price)
+      
+      current_profit = comp_price - buy_price
+      
+      if current_profit > best_profit
+        buy_day = day
+        sell_day = comp_day
+        best_profit = current_profit
+      end
 
-    stocks.each_with_index do |next_price, next_day|
-      next unless next_day == day + comparison
-      puts "Comparing Day #{day + 1} with Day #{next_day + 1}"
       comparison += 1
     end
   end
-end
 
-stock_picker([17,3,6,9,15,8,6,1,10])
+  [buy_day, sell_day]
+end
