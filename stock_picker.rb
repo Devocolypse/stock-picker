2eabc0888f4a9e4aa07e083d63dd7b6a31a01f21
Devocolypse <72180176+Devocolypse@users.noreply.github.com>
2025-05-14 09:33:36 -0700

    feat: Iterate over each day and print stock price

--- stock_picker.rb
+++ stock_picker.rb
@@ -2,4 +2,10 @@ def stock_picker(stocks)
   buy_day = 0
   sell_day = 0
   best_profit = 0
+
+  stocks.each_with_index do |price, day|
+    puts "Day #{1 + day} price: $#{price}"
+  end
 end
+
+stock_picker([17,3,6,9,15,8,6,1,10])
